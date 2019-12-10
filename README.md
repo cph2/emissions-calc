3288a4ed99a5f0255829613fc7baa85dd6c15ac3
Cheryl Handsaker <cph2@williams.edu>
2019-12-10 10:26:31 -0500

    Refactored to rewrite each line with an item code. Updated the mileage processing to handle credits more reasonably and to add an output file for manual review.

--- README.md
+++ README.md
@@ -15,7 +15,16 @@ Zilkha Center
 
 # Instructions
 ## Input
-Replace the file travel.csv in the /input folder with your file. 
+Replace the file travel.csv in the /input folder with your file.
+Maggie Koperniak is the source for the data.
+Data includes baggage fees marked with "XAO", "XAA", and "FEE".
+
+__IMPORTANT:__ Data should be sorted in the following order:
+1. Passenger Name ASC then by
+2. Merchant (ASC) then by
+3. Origin City (ASC) then by
+4. Desination City (ASC) then by
+5. Amount (__DESC__)
 
 ## Execute
 Run on the command line "php index.php"
@@ -23,12 +32,22 @@ Run on the command line "php index.php"
 Produces a summary of the mileage, broken out by train and airline travel.
 
 #Output
-There are two detailed files produced in the /output directory
+There are four detailed files produced in the /output directory
 
-**mileage.csv** is a file of the calculated mileage.
+**processed.csv** is the file that outputs each line in the input file with a status code indicating how it was interpreted.
+- __P__ - Processed and included in the mileage calculation
+- __C__ - Credit without a corresponding charge. No mileage is added or subtracted.
+- __N__ - No miles calculated. This indicates the origin and destination city are the same.
+- __I__ - Incidentals. This line assumes these charges are for baggage fees, seat upgrades and meals.
+- __M__ - Manual review needed.
+- __X__ - Coded incidenentals using "XAO", "XAA", and "FEE".
+- __E__ - Error indicates that one of the fields required for processing is missing. Typically this is the origin or destination city.
+- __U__ - Unused. Indicated this line was somehow missed in the processing.
 
-**unprocessed.csv** is a file of the errors encountered.
+**manualreview.csv** is a file for items that require human review in context. 
 
+**unprocessed.csv** is a file of the errors encountered, such as missing origin or destination city. 
  
+**mileage.csv** is a file of the rows that go into the calculated mileage.
 
 
--- index.php
+++ index.php
@@ -22,6 +22,28 @@ function getDistanceBetweenPoints($lat1, $lon1, $lat2, $lon2) {
     return round($miles);
 }
 
+function calculateMileage ($amt, $from, $to, $source) {
+
+    $mileageMultiplier = 1;
+
+    if ($amt > 0) {
+        $mileageMultiplier = 1;
+    } else if ($amt > -100) {
+        $mileageMultiplier = 0;
+    }  else if ($amt <= -100) {
+        $mileageMultiplier = -1;
+    }
+
+    //Get the latitude and longitude for the origin and destination
+    $fromLat = $source[$from]['lat'];
+    $fromLon = $source[$from]['lng'];
+    $toLat = $source[$to]['lat'];
+    $toLon = $source[$to]['lng'];
+
+    //Do the distance calculation
+    return (getDistanceBetweenPoints($fromLat, $fromLon, $toLat, $toLon) * $mileageMultiplier);
+}
+
 /**
  *  Based on @link http://gist.github.com/385876
  *  Uses the first column as the index of the associative array
@@ -53,6 +75,8 @@ function csv_to_array($filename='', $delimiter=',')
     return $data;
 }
 
+
+
 /**
  *
  * Airport data from http://ourairports.com/data/
@@ -61,18 +85,28 @@ function csv_to_array($filename='', $delimiter=',')
 **/
 $mileageOutput = fopen("output/mileage.csv", "w") or die("Unable to open mileage file!");
 $errorOutput = fopen("output/unprocessed.csv", "w") or die("Unable to open error file!");
+$manualOutput = fopen("output/manualreview.csv", "w") or die("Unable to open error file!");
+$processedOutput = fopen("output/processed.csv", "w") or die("Unable to open error file!");
 $travels = fopen('input/travel.csv', 'r');
 $airports = csv_to_array('airports.csv');
 $amtrak = csv_to_array('amtrak.csv');
 $to = '';
 $from = '';
 $dept = '';
-$id = '';
+$merchant = '';
 $totalAir = 0.0;
 $totalTrain = 0.0;
 $lineNum = 0;
 $errorLines = 0;
-$org ='';
+$manualLines = 0;
+$amt = 0;
+$prevName = "";
+$prevTo = "";
+$prevFrom = "";
+$prevAmount = 0;
+$prevMerchant = "";
+$mileage = 0;
+$statusCode = "U";
 
 
 // Process each line of the provided file
@@ -84,69 +118,80 @@ while (($line = fgetcsv($travels)) !== FALSE) {
     if ($lineNum == 0) {
 
         /* Check the column names against the CSV */
-        if (array_search('MC_CITYOFORIGIN', $line) === FALSE) {
-            echo ('Please replace the origin city code column name with MC_CITYOFORIGIN
+        if (array_search('City Of Origin', $line) === FALSE) {
+            echo('Please replace the origin city code column name with "City Of Origin"
 ');
             exit;
         }
 
-        if (array_search('MC_CITYOFDEST', $line) === FALSE) {
-            echo ('Please replace the destination city code column name with MC_CITYOFDEST
+        if (array_search('City Of Dest', $line) === FALSE) {
+            echo('Please replace the destination city code column name with "City Of Dest"
 ');
             exit;
         }
 
-        if (array_search('DEPTNAME', $line) === FALSE) {
-            echo ('Please replace the department name column name with DEPTNAME
+        if (array_search('Dept', $line) === FALSE) {
+            echo('Please replace the department name column name with Dept
 ');
             exit;
         }
 
-        if (array_search('PROJECT_ID', $line) === FALSE) {
-            echo ('Please replace the project_id column name with PROJECT_ID
+        if (array_search('Merchant', $line) === FALSE) {
+            echo('Please replace the merchant id column name with Merchant
 ');
             exit;
         }
 
-        if (array_search('MERCHANT', $line) === FALSE) {
-            echo ('Please replace the merchant id column name with MERCHANT
+        if (array_search('Amount', $line) === FALSE) {
+            echo('Please replace the transaction amount column name with "Amount"
 ');
             exit;
         }
 
+        if (array_search('Passenger Name', $line) === FALSE) {
+            echo('Please replace the passenger name column name with "Passenger Name"
+');
+            exit;
+        }
 
         // Get the indexes of the columns based on column name
         /*
         * THESE ITEMS MIGHT CHANGE, YOU SHOULD CHECK TO MAKE SURE THEY MATCH
         * IF NOT, CHANGE THE TEXT BELOW
         */
-        $from = array_search('MC_CITYOFORIGIN', $line);
-        $to = array_search('MC_CITYOFDEST', $line);
-        $dept = array_search('DEPTNAME', $line);
-        $id = array_search('PROJECT_ID', $line);
-        $merchant = array_search('MERCHANT', $line);
-        //$org = array_search('ORIGINCITY', $line);
-        //$dest = array_search('DESTINATION', $line);
+        $from = array_search('City Of Origin', $line);
+        $to = array_search('City Of Dest', $line);
+        $dept = array_search('Dept', $line);
+        $merchant = array_search('Merchant', $line);
+        $amt = array_search('Amount', $line);
+        $name = array_search('Passenger Name', $line);
 
         // Count the line numbers to make it easy to check errors
         $lineNum = 1;
 
         // Set up the headers in the output and error files
-        $outputHeader = "Dept,Project Id,Mileage\n";
+        $outputHeader = "Dept,Passenger,Origin,Destination,Mileage,Amount\n";
         fwrite($mileageOutput, $outputHeader);
-        $errorHeader = "line,id,origin,destination\n";
+        $errorHeader = "line, dept,passenger,origin,destination,amount\n";
         fwrite($errorOutput, $errorHeader);
+        $manualHeader = "line,dept,passenger,origin,destination,amount\n";
+        fwrite($manualOutput, $manualHeader);
+        $processedHeader = "";
+        foreach ($line as $linefield) {
+            $processedHeader .= ($linefield . ",");
+        }
+        $processedHeader .= "mileage, process-code\n";
+        fwrite($processedOutput, $processedHeader);
 
-    } else {
 
-        // Process each line in the file
+    } else {  // Process each line in the file
 
         //Increment the line number
-        $lineNum = $lineNum+1;
+        $lineNum = $lineNum + 1;
 
         // Assume it is air travel unless AMTRAK is specified
         $mileageSource = $airports;
-        if (strpos( $line[$merchant], 'AMTRAK' ) !== false) {
+        if (strpos($line[$merchant], 'AMTRAK') !== false) {
             $mileageSource = $amtrak;
         }
 
@@ -156,29 +201,66 @@ while (($line = fgetcsv($travels)) !== FALSE) {
             // Make sure that the origin and destination are not the same
             if ($line[$from] != $line[$to]) {
 
-                //Get the latitude and longitude for the origin and destination
-                $fromLat = $mileageSource[$line[$from]]['lat'];
-                $fromLon = $mileageSource[$line[$from]]['lng'];
-                $toLat = $mileageSource[$line[$to]]['lat'];
-                $toLon = $mileageSource[$line[$to]]['lng'];
+                // Identify lines for manual inspection
+                if (($line[$name] == $prevName) && ($line[$from] == $prevFrom) && ($line[$to] == $prevTo) && ($merchant == $prevMerchant) && ($line[$amt] != $prevAmount)  && ($mileageSource != $amtrak)) {
+                    if ($line[$amt] > 100) { //Should be checked manually
+                        $manualLines = $manualLines + 1;
+                        $outputLine = $lineNum . ',' . str_replace(",", " ", $line[$dept]) . ',' . $line[$name] . ',' . $line[$from] . ',' . $line[$to] . ',$' . $line[$amt] . "\n";
+                        fwrite($manualOutput, $outputLine);
+                        $statusCode = "M";
+                    } else if (($line[$amt] <= 100) && ($line[$amt] >= -100)){  //Assume these are travel incidentals
+                        $errorLines = $errorLines + 1;
+                        $outputLine = $lineNum . ',' . str_replace(",", " ", $line[$dept]) . ',' . $line[$name] . ',' . $line[$from] . ',' . $line[$to] . ',$' . $line[$amt] . "\n";
+                        fwrite($errorOutput, $outputLine);
+                        $statusCode = "I";
+                    } else if ($line[$amt] < -100) { //Assume these are refunds
+                        $tripMileage = calculateMileage($line[$amt], $line[$from], $line[$to], $mileageSource);
 
-                //Do the distance calculation
-                $tripMileage = getDistanceBetweenPoints($fromLat, $fromLon, $toLat, $toLon);
+                        // Save the mileage to write to the processing file
+                        $mileage = $tripMileage;
+                        $statusCode = "P";
 
-                //Write the record
-                $outputTrip = $line[$dept] . ", " . $line[$id] . ", " . $line[$from] . ", " . $line[$to] . ", " .$tripMileage . "\n";
-                fwrite($mileageOutput, $outputTrip);
+                        //Write the record
+                        $outputTrip = str_replace(",", " ", $line[$dept]) . ", " . $line[$name] . ", " . $line[$from] . ", " . $line[$to] . ", " . $tripMileage . ", $" . $line[$amt] . "\n";
+                        fwrite($mileageOutput, $outputTrip);
 
-                // Update the total mileage
-                if ($mileageSource == $amtrak){
-                  $totalTrain = $totalTrain + $tripMileage;
-                } else {
-                  $totalAir = $totalAir + $tripMileage;
+                        // Update the total mileage
+                        if ($mileageSource == $amtrak) {
+                            $totalTrain = $totalTrain + $tripMileage;
+                        } else {
+                            $totalAir = $totalAir + $tripMileage;
+                        }
+                    }
+
+                } else { // Calculate the mileage
+
+                    if ($line[$amt] > 0) {
+
+                        $tripMileage = calculateMileage($line[$amt], $line[$from], $line[$to], $mileageSource);
+
+                        // Save the mileage to write to the processing file
+                        $mileage = $tripMileage;
+                        $statusCode = "P";
+
+                        //Write the record
+                        $outputTrip = str_replace(",", " ", $line[$dept]) . ", " . $line[$name] . ", " . $line[$from] . ", " . $line[$to] . ", " . $tripMileage . ", $" . $line[$amt] . "\n";
+                        fwrite($mileageOutput, $outputTrip);
+
+                        // Update the total mileage
+                        if ($mileageSource == $amtrak) {
+                            $totalTrain = $totalTrain + $tripMileage;
+                        } else {
+                            $totalAir = $totalAir + $tripMileage;
+                        }
+                    }
+                    else {
+                        $statusCode = "C"; //Assume credit for travel not taken
+                    }
                 }
+            } else {
+                $statusCode = "N"; //Source and destination are the same, no mileage calculated.
             }
-        }
-
-        else {
+        } else {
             // Remove fees and other similar things from data
             /*
             * THIS MAY NEED TO BE UPDATED
@@ -187,27 +269,57 @@ while (($line = fgetcsv($travels)) !== FALSE) {
             */
             if (($line[$from] != "XAA") && ($line[$to] != "XAO") && ($line[$to] != "FEE") && ($line[$from] != "FEE")) {
 
-                // This is badly formatted or needs a lookup so write it to the file
-                $errorLines = $errorLines + 1;
-                $outputLine = $lineNum . ',' . $line[$id] . ',' . $line[$from] . ',' . $line[$to] . ',' . $line[$org] . ',' . $line[$dest] . "\n";
-                fwrite($errorOutput, $outputLine);
+            // This is badly formatted or needs a lookup so write it to the file
+            $errorLines = $errorLines + 1;
+            $outputLine = $lineNum . ',' . str_replace(",", " ", $line[$dept]) . ',' . $line[$name] . ',' . $line[$from] . ',' . $line[$to] . ',$' . $line[$amt] . "\n";
+            fwrite($errorOutput, $outputLine);
+
+            $statusCode = "E"; // Origin and/or destination missing.
+
+            } else {
+               $statusCode = "X"; // One of the fake codes entered for fees and incidentals.
             }
+
         }
+
+        // Write out the processed record
+        $procLine = "";
+        foreach ($line as $linefield) {
+            $procLine .= (str_replace(",", " ", $linefield) . ",");
+        }
+        $procLine .= $mileage. ",". $statusCode ."\n";
+        fwrite($processedOutput, $procLine);
+
+
+        $prevName = $line[$name];
+        $prevAmount = $line[$amt];
+        $prevFrom = $line[$from];
+        $prevTo = $line[$to];
+        $prevMerchant = $merchant;
+        $mileage = 0;
+        $statusCode = 'U';
     }
 }
 
 //Write the total mileage to the bottom of the mileage file
-$outputTotal = "Results:\nAir Miles: " . $totalAir . "\nTrain Miles: " . $totalTrain . "\n";
+    $outputTotal = "Results:\nAir Miles: " . $totalAir . "\nTrain Miles: " . $totalTrain . "\n";
 
 // If there are error lines, write the total out to the bottom of the mileage file
-if ($errorLines > 0) {
-    $outputTotal .= $errorLines . " Unprocessed Lines\n";
-}
+    if ($errorLines > 0) {
+        $outputTotal .= $errorLines . " Unprocessed Lines\n";
+    }
 
-echo $outputTotal;
-fwrite($mileageOutput, $outputTotal);
+// If there are error lines, write the total out to the bottom of the mileage file
+    if ($manualLines > 0) {
+        $outputTotal .= $manualLines . " Manual Review Lines\n";
+    }
+
+
+    echo $outputTotal;
+    fwrite($mileageOutput, $outputTotal);
 
 // Close all the files
-fclose($travels);
-fclose($mileageOutput);
-fclose($errorOutput);
+    fclose($travels);
+    fclose($mileageOutput);
+    fclose($errorOutput);
+    fclose($manualOutput);
